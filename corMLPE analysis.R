bce108da53bfab0dc0d185660d859d13433199d6
freshwaterelasmo-guy <samuelamini20@gmail.com>
2024-09-26 20:12:02 +0930

    Good to go for final analysis with temporal component...different models will be of value next year.

--- corMLPE analysis.R
+++ corMLPE analysis.R
@@ -9,6 +9,11 @@ install.packages("bbmle")
 install.packages("ggeffects")
 install.packages("usdm")
 install.packages("paletteer")
+install.packages("performance")
+install.packages("emmeans")
+install.packages("MuMIn")
+install.packages("rcompanion")
+install.packages("ggsignif")
 
 ## --- START HERE PEOPLES --- ##
 
@@ -20,6 +25,12 @@ library(bbmle)
 library(ggeffects)
 library(tidyverse)
 library(paletteer)
+library(performance)
+library(emmeans)
+library(lme4)
+library(MuMIn)
+library(rcompanion)
+library(ggsignif)
 
 ## --- NOW WE HAVE EVERYTHING LOADED --- ##
 
@@ -29,31 +40,45 @@ summary(data)
 
 data
 
-data2 <- data %>% rename(genet_dist = value,
+data2 <- data %>% rename(relatedness = value,
                 geogr_dist = dist, 
                 ID1 = id_1, 
                 ID2 = id)
 
 head(data2)
+
+ nlevels(data2$ID1)
+ summary(data2$genet_dist)
+ sd(data2$genet_dist)
+ 
+ nrow(data2)
+ levels(data2$Billabong_ID1)
+ 
+ levels(data2$billabong_both)
+ 
+ data2$billabong_both <- recode_factor(data2$billabong_both, "Bend:Bank" = "River:Bend", "DalyRiv_Rd:Bend" = "River:Bend", "River:DalyRiv_Rd" = "River:River", "DalyRiv_Rd:Fish" = "River:Fish", "DalyRiv_Rd:Bank" = "River:River", "River:Bank" = "River:River", "Bank:Fish" = "River:Fish")
+ 
+levels(data2$billabong_both)
+
 ## Remove the comparisons between individuals with no date of capture...
 
-data2 <- data2[-c(1:55),]
+data2 <- data2[-c(1:55),] ## only use for some models 
 
-ggplot(data = data2, aes(x = geogr_dist, y = genet_dist, colour = billabong_diff, shape = year_caught_diff)) +
+ggplot(data = data2, aes(x = geogr_dist, y = relatedness, colour = billabong_diff, shape = year_caught_diff)) +
   geom_jitter(stat = "identity", size = 3, width = 0.3)+
   theme_classic()+
   xlab("Geogrpahic dist (km)")
 
-ggplot(data = data2, aes(x = year_caught_diff, y = genet_dist)) +
+ggplot(data = data2, aes(x = year_caught_diff, y = relatedness)) +
   geom_boxplot() +
   geom_jitter(aes(colour = geogr_dist), width = 0.25, size = 3)+
   theme_classic()
 
-ggplot(data = data2, aes(x = billabong_diff, y = genet_dist)) +
+ggplot(data = data2, aes(x = billabong_diff, y = relatedness)) +
   geom_boxplot() +
   geom_jitter( size = 3)
 
-ggplot(data = data2, aes(x = catch_set_diff, y = genet_dist)) +
+ggplot(data = data2, aes(x = catch_set_diff, y = relatedness)) +
   geom_boxplot() +
   geom_jitter( size = 3, aes(colour = billabong_diff), width = 0.2, alpha = 0.7)+
   theme_classic()
@@ -66,8 +91,8 @@ ggplot(data = data2, aes(x = catch_set_diff, y = genet_dist)) +
 # also check out distribution of genetic distance - approx. normal or strongly skewed?
 ## if latter, might need to transform first (e.g. log if right skewed)...
 
-hist(data2$genet_dist)
-hist(log(data2$genet_dist))
+hist(data2$relatedness)
+hist(log(data2$relatedness))
 
 data <- data2[-c(1:55),]
 
@@ -75,9 +100,9 @@ data <- transform(na.omit(data))
 
 summary(data)
 
-hist(log(data$genet_dist))
+hist(log(data$relatedness))
 
-data2$log_GenD <- log(data2$genet_dist)
+data2$log_GenD <- log(data2$relatedness)
 
 ggplot(data = data2, aes(x = year_caught_diff, y = log_GenD)) +
   geom_boxplot() +
@@ -91,7 +116,7 @@ hist(data2$geogr_dist)
 
 
 ## log()
-m1 <- gls(genet_dist ~ year_caught_diff + scale(geogr_dist), 
+m1 <- gls(log(relatedness + 0.001) ~ year_caught_diff, 
           data = data2, 
           correlation = corMLPE(form = ~ID1 + ID2), 
           method = "REML", 
@@ -99,34 +124,45 @@ m1 <- gls(genet_dist ~ year_caught_diff + scale(geogr_dist),
           control = list(singular.ok = T))
 
 
-# use categorical variable instead 
-m2 <- gls(genet_dist ~ scale(geogr_dist), 
+m2 <- gls(log(relatedness+0.001) ~ scale(geogr_dist), 
                 data = data2, 
                 correlation = corMLPE(form = ~ID1 + ID2), 
                 method = "REML", 
           control = list(singular.ok = T))
 
-m3 <- gls(log(genet_dist) ~ year_caught_diff + scale(geogr_dist), 
+m3 <- gls(log(relatedness+0.001) ~ scale(geogr_dist) + year_caught_diff, 
           data = data2, 
           correlation = corMLPE(form = ~ ID1 + ID2), 
           method = "REML", 
           control = list(singular.ok = T))
 
-m4 <- gls(genet_dist ~ billabong_diff, 
+m4 <- gls(log(relatedness+0.001) ~ billabong_diff, 
           data = data2, 
           correlation = corMLPE(form = ~ID1 + ID2), 
           method = "REML", 
           na.action = "na.omit", 
           control = list(singular.ok = T))
 
-m5 <- gls(genet_dist ~ catch_set_diff, 
+m5 <- gls(log(relatedness+0.001) ~ catch_set_diff, 
           data = data2, 
           correlation = corMLPE(form = ~ID1 + ID2), 
           method = "REML", 
           na.action = "na.omit", 
           control = list(singular.ok = T))
 
-m6 <- gls(genet_dist ~ billabong_diff*year_caught_diff, 
+m6 <- gls(log(relatedness+0.001) ~ billabong_diff*year_caught_diff, 
+          data = data2, 
+          correlation = corMLPE(form = ~ID1 + ID2), 
+          method = "REML", 
+          control = list(singular.ok = T))
+
+m7 <- gls(log(relatedness+0.001) ~ billabong_both, 
+          data = data2, 
+          correlation = corMLPE(form = ~ID1 + ID2), 
+          method = "REML", 
+          control = list(singular.ok = T))
+
+m8 <- gls(log(relatedness+0.001) ~ year_caught_both, 
           data = data2, 
           correlation = corMLPE(form = ~ID1 + ID2), 
           method = "REML", 
@@ -135,43 +171,40 @@ m6 <- gls(genet_dist ~ billabong_diff*year_caught_diff,
 # include year e.g. as random effect if many levels -...
 #.... change model to lme() i.e. mixed effect model
 
-#lme1 <- lme(genet_dist ~ catch_set_diff,
-          data = data2, 
-          random = ~1|#WHAT TO PUT HERE??, 
-          corMLPE(form = ~ID1 + ID2|year_caught_diff), 
-          method = "REML")
-
-# compare model fit with AICc --------------
-
-tab <- AICctab(m1, m2, m3, m4, m5, m6,  weights=TRUE)
+tab <- AICctab(m1, m2, m3, m4, m5, m6, m7, m8, weights=TRUE)
 
 tab
 
 write.csv(tab, "AICcTable.csv")
 
 # model summary + plot ---------------
-summary(m5)
-anova(m5)
 
-summary(m4)
-anova(m4)
+## In order of best fit 
+
+dev.off()
+
+#1
+summary(m7) 
+anova(m7)
+confint(m7)
+plot(m7)
+
+plot(m4, resid(., type="normalized") ~ fitted(.), abline = c(0,0))
+
+ggplot(data2, aes(y=resid(m4, type="normalized"), 
+                  x= billabong_diff)) + 
+  geom_boxplot()
+
+nagelkerke(m4) ## Note that the mdoel was refitted with ML not REML 
+
+## include but don't rely on
 
 # 95% CI's are back-transformed i.e. can ignore warning about SE's being on the log scale
 
-ggpredict(model = m4, terms = c("billabong_diff")) %>%  
+
+ggpredict(model = m4, terms = c("billabong_diff"), back_transform = TRUE) %>%  
   plot(show_data = TRUE, jitter = 0.07)
 
-
-ggpredict(model = m5, terms = c("catch_set_diff")) %>%  
-  plot(show_data = TRUE, jitter = 0.07)
-
-# check model residuals - no patterns across fitted values nor predictors -----
-
-plot(m5, resid(., type="normalized") ~ fitted(.), abline = c(0,0))
-
-ggplot(data2, aes(y=resid(m5, type="normalized"), 
-                 x= catch_set_diff)) + 
-  geom_boxplot()
 
 ## --- Plot for aesthetics --- ## 
 
@@ -189,4 +222,43 @@ plot1 <- plot1 + theme(plot.title = element_text(hjust = 0.5, size = 15),
                        axis.text = element_text(size = 11))
 
 plot1
+
+###-------------------------------------------------------------------------
+
+## Clean the palette 
+dev.off()
+
+## Plot 2
+
+plot2 <- ggplot(data = data2, aes(x = reorder(billabong_diff, -log(relatedness + 0.001)), y = log(relatedness + 0.001), fill = billabong_both))+
+  geom_boxplot()+
+  geom_signif(data=data2,comparisons=list( c("River:Fish", "Bend:Fish"),c("River:Fish", "Fish:Fish"), c("River:River","River:Fish")),
+              map_signif_level = TRUE, 
+              y_position = c(-1.3, -1.0, -0.70, -0.40), 
+              annotations = c("*", "***", "***"), 
+              textsize = 4.5) +
+  ggtitle("Relatedness Between Billabongs") +
+  xlab("Pairwise Billabong Comparisons") +
+  ylab("Log ( Relatedness + 0.001 )") +
+  theme_bw()
+
+plot2 <- plot2 + theme(plot.title = element_text(hjust = 0.5)) +
+  guides(fill=guide_legend(title="Billabong Comparison"))
+
+print(plot2)
+
+#geom_hline(yintercept = mean(log(data2$relatedness + 0.001)), ) +
+
+## Post hoc comparisons --
+
+emm <- emmeans(m7, ~ billabong_both)
+simp <- pairs(emm, simple = "each")
+simp
+
+install.packages("multcompView")
+library(multcompView)
+tukey <- TukeyHSD(aov(mod), "billabong_both")
+plot(tukey , las=1 , col="brown", las = 2)
+
+#model effect jtools 
 
